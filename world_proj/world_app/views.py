4800e25d34c9a51f91a9f4e4e87a0764299d3d95
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-03-24 14:50:57 +0530

    Updated so that it takes in search query and returns respective table

--- world_proj/world_app/views.py
+++ world_proj/world_app/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import City, Country, Countrylanguage
 from .admin import cityAdmin,countryAdmin,countrylanguageAdmin
 
-
-
 def index(request):
     city=City.objects.all()
     country=Country.objects.all()
@@ -11,6 +9,7 @@ def index(request):
     city_attributes=cityAdmin.list_display
     country_attributes=countryAdmin.list_display
     countrylanguage_attributes=countrylanguageAdmin.list_display
+    query=request.GET.get('query')
     context={
         'city':city,
         'country':country,
@@ -18,5 +17,6 @@ def index(request):
         'city_attributes':city_attributes,
         'country_attributes':country_attributes,
         'countrylanguage_attributes':countrylanguage_attributes,
+        'query':query,
     }
     return render(request, 'world_app/index.html',context)
